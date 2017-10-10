60b8042841dfb753633dfb2fc752b6b5e143af5a
zfeng1101 <webzfeng@163.com>
2017-10-10 08:13:11 -0700

    项目第二天内容

---
+++ db/subjectDB.js
@@ -0,0 +1,54 @@
+require('babel-polyfill');
+let pool = require('./pool');
+module.exports = {
+	//获取所有类型数据
+	findAlltype(){
+		var sql = "select * from tbl_exam_subjecttype";
+		return pool.execute(sql);
+	},
+
+	//查询所有难易程度数据
+	findAlllevel(){
+		var sql = "select * from tbl_exam_subjectlevel";
+		return pool.execute(sql);
+	},
+
+	//查询所有方向数据
+	findAlldepartment(){
+		var sql = "select * from tbl_exam_epartment";
+		return pool.execute(sql);
+	},
+
+	//查询所有知识点数据
+	findAlltopic(){
+		var sql = "select * from tbl_exam_topic";
+		return pool.execute(sql);
+	},
+
+	//查询题目
+	findSubject(type,level,epartment,topic){
+		console.log(type,level,epartment,topic);
+		// var sql = "select * from tbl_exam_subject where department_id = "
+		// +epartment+" and  subjectLevel_id = "
+		// +level+" and subjectType_id = "
+		// +type+" and topic_id = "+topic+" ";
+		var sql = "select tbl_exam_subjecttype.realName as tName,tbl_exam_subjectlevel.realName as lName,tbl_exam_subject.* from tbl_exam_subject,tbl_exam_subjectlevel,tbl_exam_subjecttype where tbl_exam_subject.subjectLevel_id = tbl_exam_subjectlevel.id and tbl_exam_subject.subjectType_id = tbl_exam_subjecttype.id and department_id = "
+		+epartment+" and  subjectLevel_id = "
+		+level+" and subjectType_id = "
+		+type+" and topic_id = "+topic+" ";
+		return pool.execute(sql);
+	},
+
+	//查询出选项
+	findContent(id){
+		var sql = "select content from tbl_exam_choice where subject_id = "+id;
+		return pool.execute(sql);
+	},
+
+	//审核
+	checkState(id,checkState){
+		var sql = "update tbl_exam_subject set checkState = '"+checkState+"' where id = "+id+" ";
+		return pool.execute(sql);
+	}
+
+}
---
+++ routes/subjectRoute.js
@@ -0,0 +1,94 @@
+require('babel-polyfill');
+var express = require('express');
+var route = express.Router();
+var subjectDB = require('../db/subjectDB');
+
+//查询所有类型数据
+route.get('/findAlltype',(req,resp)=>{
+	console.log("+++++++++++++++++++++++++++++++","类型");
+	subjectDB.findAlltype().then((data)=>{
+		console.log("----------------",data);
+		resp.send(data);
+	}).catch((error)=>{
+		resp.send(error);
+	});
+});
+
+
+//查询所有难易程度数据
+route.get('/findAlllevel',(req,resp)=>{
+	console.log("+++++++++++++++++++++++++++++++","难易程度");
+	subjectDB.findAlllevel().then((data)=>{
+		console.log("----------------",data);
+		resp.send(data);
+	}).catch((error)=>{
+		resp.send(error);
+	});
+});
+
+
+//查询所有方向数据
+route.get('/findAlldepartment',(req,resp)=>{
+	console.log("+++++++++++++++++++++++++++++++","方向");
+	subjectDB.findAlldepartment().then((data)=>{
+		console.log("----------------",data);
+		resp.send(data);
+	}).catch((error)=>{
+		resp.send(error);
+	});
+});
+
+
+//查询所有知识点数据
+route.get('/findAlltopic',(req,resp)=>{
+	console.log("+++++++++++++++++++++++++++++++","知识点");
+	subjectDB.findAlltopic().then((data)=>{
+		console.log("----------------",data);
+		resp.send(data);
+	}).catch((error)=>{
+		resp.send(error);
+	});
+});
+
+
+//查询所有已有题目
+route.post('/findSubject',(req,resp)=>{
+	console.log("++++++++++++++++++++++++++++++++++","选中的题目");
+	var type = req.body.type;
+	var level = req.body.level;
+	var epartment = req.body.epartment;
+	var topic = req.body.topic;
+	console.log(type,level,epartment,topic);
+	subjectDB.findSubject(type,level,epartment,topic).then((data)=>{
+		console.log("-------有沙滩",data);
+		resp.send(data);
+	}).catch((error)=>{
+		resp.send(error);
+	});
+});
+
+
+//查询选项
+route.post('/findContent',(req,resp)=>{
+	var id = req.body.id;
+	subjectDB.findContent(id).then((data)=>{
+		resp.send(data);
+	}).catch((error)=>{
+		resp.send(error);
+	});
+})
+
+
+//审查
+route.post('/checkState',(req,resp)=>{
+	var id = req.body.id;
+	var checkState = req.body.checkState;
+	console.log(id,checkState);
+	subjectDB.checkState(id,checkState).then((data)=>{
+		resp.send(data);
+	}).catch((error)=>{
+		resp.send(error);
+	});
+});
+
+module.exports = route;
